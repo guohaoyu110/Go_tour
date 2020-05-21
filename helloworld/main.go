0127a716b8d17a8b653836560d0466a0f149186e
guohaoyu110 <guohaoyu110@gmail.com>
2020-05-21 15:22:04 -0700

    add hello-world go file

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,14 @@
 package main
 
+// what does package main meane?
+// package = project = workspace
+// package main is used to build an excutable package
+// executable这样可以生成执行文件，其他的package不能生成执行文件
 import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
 }
+
+// waht does import "fmt" mean?
+// fmt is standard library package
